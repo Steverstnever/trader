1470a3945805033d46e4f94c24fa6d39e80407c0
liujiachen <1047382370@qq.com>
2020-12-04 14:43:35 +0800

    [add]添加telegram通知器

--- trader/notifier/__init__.py
+++ trader/notifier/__init__.py
@@ -1,6 +1,8 @@
 import logging
 from abc import ABC, abstractmethod
 
+from telegram.ext import Updater
+
 
 class Notifier(ABC):
     """
@@ -26,3 +28,17 @@ class LoggerNotifier(Notifier):
 
     def notify(self, msg: str):
         self.log.warning(f"【通知】{msg}")
+
+
+class TgNotifier(Notifier):
+    """
+    telegram 通知
+    """
+
+    def __init__(self, token: str, chat_id: int):
+        self._token = token
+        self._chat_id = chat_id
+
+    def notify(self, msg: str):
+        updater = Updater(token=self._token, use_context=True)
+        updater.bot.send_message(self._chat_id, f"【通知】{msg}", timeout=5)
