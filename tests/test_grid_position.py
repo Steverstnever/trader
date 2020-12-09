94a6e2ae28481500dbf24c42cf38bf4398804ad7
liujiachen <1047382370@qq.com>
2020-12-09 14:05:29 +0800

    [add]添加volumeprofile网格

--- tests/test_grid_position.py
+++ tests/test_grid_position.py
@@ -1,10 +1,13 @@
 import math
+import time
+import random
 from decimal import Decimal
 
 import pytest
 
 from trader.strategy.grid.grid_generators import ArithmeticGridGenerator, GeometricGridGenerator, MixedGridGenerator, \
-    assert_grid_levels
+    assert_grid_levels, KBar, VolumeGridGenerator
+from trader.strategy.grid.grid_utils import create_volume_profile
 from trader.strategy.grid.grid_position_manager import Level, LevelPositionError, GridPositionManager
 from trader.utils import is_constant
 
@@ -186,3 +189,21 @@ def test_grid_manager():
     assert grid.get_positions_to_buy(current_price=Decimal('1.5')) == Decimal('5')
     assert grid.get_positions_to_buy(current_price=Decimal('2.5')) == Decimal('0')
     assert grid.get_positions_to_buy(current_price=Decimal('3.5')) == Decimal('0')
+
+
+def test_volume_profile_grid():
+    k_line = list()
+    for i in range(300):
+        low = random.uniform(i + 10, i + 20)
+        high = random.uniform(i + 20, i + 30)
+        open_ = random.uniform(low, high)
+        close = random.uniform(low, high)
+        vol = random.uniform(1, 300)
+        k_line.append(KBar(id_=str(time.time()), open=Decimal(open_),
+                           close=Decimal(close), high=Decimal(high),
+                           low=Decimal(low),
+                           vol=Decimal(vol)))
+    g = VolumeGridGenerator(Decimal("50.0001"), Decimal("257.02"), 10,
+                            Decimal("200"), create_volume_profile(k_line))
+    for level in g.generate():
+        print(level)
